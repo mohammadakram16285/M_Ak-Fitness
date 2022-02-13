e2054a2e51027d20febb9d7f3e571529eb7aea4c
Mohammad Akram <qariakram13@gmail.com>
2022-02-13 18:33:00 +0300

    info card design

--- public/js/main.js
+++ public/js/main.js
@@ -2,6 +2,13 @@ const closeBtn = document.querySelector('.closeBtn');
 const toggleMenu = document.querySelector('.toggleMenu');
 const menu = document.querySelector('.menu');
 
+
+//info HTML
+
+const previous_icon = document.getElementById('previous_icon');
+const next_icon = document.getElementById('next_icon');
+const infocard = document.querySelector('.infocard');
+
 toggleMenu.addEventListener('click',()=> {
     menu.classList.add('show')
 
@@ -13,4 +20,14 @@ closeBtn.addEventListener('click',()=> {
 });
 
 
+next_icon.addEventListener('click', ()=> {
+    console.log('clicked')
+    infocard.scrollLeft += 240;
+});
+
+previous_icon.addEventListener('click', ()=> {
+    console.log('clicked')
+    infocard.scrollLeft -= 240;
+
+});
 
